3ddb79ad148fa657ce45b4d35da01ec6290b6f56
Guillermo Lella <arkorott@gmail.com>
2018-10-14 15:41:34 -0400

    updated coroutine code

--- Kotlin/chapter_14_challenge_room_database_coroutines/app/src/main/java/com/glella/criminalintent/CrimeLab.kt
+++ Kotlin/chapter_14_challenge_room_database_coroutines/app/src/main/java/com/glella/criminalintent/CrimeLab.kt
@@ -3,7 +3,10 @@ package com.glella.criminalintent
 
 import com.glella.criminalintent.database.AppDatabase
 import com.glella.criminalintent.database.CrimesListDAO
-import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.experimental.Dispatchers
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -21,59 +24,62 @@ object CrimeLab {
         crimesDAO = appDB.crimesListDAO()
     }
 
-    fun getCrimes(): ArrayList<Crime> = runBlocking(Dispatchers.Default) {
-        //val result = async { retrieveCrimes() }.await()
-        val result = async { crimesDAO.getAllCrimes() }.await()
-        return@runBlocking result as ArrayList<Crime>
-    }
 
-    fun getCrime(id: UUID): Crime? = runBlocking(Dispatchers.Default) {
-        val uuidString = id.toString()
-        //var crime: Crime = crimesDAO.getOneCrime(uuidString)
-        val result = async { crimesDAO.getOneCrime(uuidString) }.await()
-        return@runBlocking result
-    }
+    // *** Suspending Private DAO Functions ***
 
-    fun addCrime(crime: Crime) = runBlocking(Dispatchers.Default) {
-        val job = GlobalScope.launch { crimesDAO.insertCrime(crime) }
-        //crimesDAO.insertCrime(crime)
-        job.join()
-        mCrimes.clear()
-        mCrimes = getCrimes()
-    }
-
-    fun updateCrime(crime: Crime) = runBlocking(Dispatchers.Default) {
-        val job = GlobalScope.launch { crimesDAO.updateCrime(crime) }
-        job.join()
-    }
-
-    fun deleteCrime(crime: Crime): Boolean = runBlocking(Dispatchers.Default) {
-        val job = GlobalScope.launch { crimesDAO.deleteCrime(crime) }
-        mCrimes.clear()
-        mCrimes = getCrimes()
-
-        return@runBlocking true
-    }
-
-
-    /* Not needed - suspend functions
-    suspend fun retrieveCrimes(): List<Crime> {
+    suspend private fun retrieveCrimesSuspending(): List<Crime> {
         return crimesDAO.getAllCrimes()
     }
 
+    suspend private fun retrieveCrimeSuspending(uuidString: String): Crime? {
+        return crimesDAO.getOneCrime(uuidString)
+    }
 
-    getCrimes() original - Main Thread
-    return crimes
-    return ArrayList()
+    suspend private fun addCrimeSuspending(crime: Crime) {
+        crimesDAO.insertCrime(crime)
+    }
 
-    Coroutines with async and await (good for more than 1 simultaneous action)
-    val result = async { retrieveCrimes() }.await()
-    return@runBlocking result as ArrayList<Crime>
+    suspend private fun updateCrimeSuspending(crime: Crime) {
+        crimesDAO.updateCrime(crime)
+    }
 
-    3rd option - does not refresh list on ListFragment
-    val result = withContext(Dispatchers.Default) { retrieveCrimes() }
-    return@runBlocking result as ArrayList<Crime>
-    */
+    suspend private fun deleteCrimeSuspending(crime: Crime) {
+        crimesDAO.deleteCrime(crime)
+    }
 
 
+    // *** Public DAO Functions ***
+
+    fun getCrimes(): ArrayList<Crime> = runBlocking {
+        val result = async(Dispatchers.Default) { retrieveCrimesSuspending() }.await()
+        return@runBlocking result as ArrayList<Crime>
+    }
+
+    fun getCrime(id: UUID): Crime? = runBlocking {
+        val uuidString = id.toString()
+        val result = async(Dispatchers.Default) { retrieveCrimeSuspending(uuidString) }.await()
+        return@runBlocking result
+    }
+
+    fun addCrime(crime: Crime) = runBlocking {
+        val job = launch(Dispatchers.Default) { addCrimeSuspending(crime) }
+        job.join()
+        mCrimes.clear()
+        mCrimes = getCrimes()
+    }
+
+    fun updateCrime(crime: Crime) = runBlocking {
+        val job = launch(Dispatchers.Default) { updateCrimeSuspending(crime) }
+        job.join()
+    }
+
+    fun deleteCrime(crime: Crime): Boolean = runBlocking {
+        val job = launch(Dispatchers.Default) { deleteCrimeSuspending(crime) }
+        job.join()
+        mCrimes.clear()
+        mCrimes = getCrimes()
+        return@runBlocking true
+    }
+
+    
 }
